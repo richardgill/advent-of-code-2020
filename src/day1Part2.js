fa582e3eefb1eacb2aa66109d3e145e5cf4a4ca6
Richard Gill <richard@rgill.co.uk>
2020-12-01 08:56:50 +0000

    part2 finished

--- src/day1Part2.js
+++ src/day1Part2.js
@@ -3,20 +3,18 @@ import _ from 'lodash'
 import { map, find, sum } from 'lodash/fp'
 
 const getPairCombosHelper = ([first, second, ...tail]) => {
-  if (!_.isNumber(first) || !_.isNumber(second)) {
+  if (!_.isFinite(first) || !_.isFinite(second)) {
     return []
   }
-  return [...tail.map((x) => [(first, second, x)]), ...getPairCombosHelper([first, ...tail])]
+  return [...(tail || []).map((x) => [first, second, x]), ...getPairCombosHelper([first, ...tail])]
 }
 const getPairCombos = ([first, ...tail]) => {
-  if (!_.isNumber(first)) {
+  if (!_.isFinite(first)) {
     return []
   }
   return [...getPairCombosHelper([first, ...tail]), ...getPairCombos(tail)]
 }
 
 const input = fs.readFileSync('./data/day1.txt').toString().split('\n') |> map(parseInt)
-
 const result = getPairCombos(input) |> find((x) => sum(x) === 2020)
-
-console.log(result[0] * result[1])
+console.log(result[0] * result[1] * result[2])
